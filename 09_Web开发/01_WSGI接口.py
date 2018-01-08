92aa35f4c2b832a3d3c1925d7f624fa96b237ddc
Jason-PC <morata@189.cn>
2018-01-08 20:02:18 +0800

    version 0.1.1 xiugai

--- 09_Web开发/01_WSGI接口.py
+++ 09_Web开发/01_WSGI接口.py
@@ -3,5 +3,5 @@ from wsgiref.simple_server import make_server
 from application import application
 
 http_00 = make_server("", 8000, application)
-print("Serving HTTP on port 8000 ...")
+print("Serving HTTP on p+ort 8000 ...")
 http_00.serve_forever()
--- 09_Web开发/application.py
+++ 09_Web开发/application.py
@@ -3,4 +3,5 @@
 def application(encion, start_response):
     start_response("200 OK", [("Content-type", "text/html")])
     body = "<h1>Hello %s!</h1>" % (encion["PATH_INFO"][1:] or "web")
+
     return [body.encode("utf-8")]
