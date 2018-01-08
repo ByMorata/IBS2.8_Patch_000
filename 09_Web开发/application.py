8d144a1d12384a47f549fbe070506bbf48446989
Jason-Moblie <crycode@outlook.com>
2018-01-08 17:33:43 +0800

    version 0.1.0 Web开发

---
+++ 09_Web开发/application.py
@@ -0,0 +1,6 @@
+# 00_application.py
+
+def application(encion, start_response):
+    start_response("200 OK", [("Content-type", "text/html")])
+    body = "<h1>Hello %s!</h1>" % (encion["PATH_INFO"][1:] or "web")
+    return [body.encode("utf-8")]
